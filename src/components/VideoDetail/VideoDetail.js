ec80908725b7fe016486fc1981574acc121826b8
ganiular <ganiular@gmail.com>
2024-02-24 10:23:32 +0100

    responsive

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -15,16 +15,19 @@ function VideoDetail({ data }) {
     return (
         <div className='video-detail'>
             <h1 className="video-detail__title">Tech Trends: {data.title}</h1>
+            <div className='divider mobile'></div>
             <div className="video-detail__tile">
                 <div className="video-detail__tile-item">
                     <h3 className='video-detail__channel'>By {data.channel}</h3>
                     <div className="video-detail__date">{formatDate(data.timestamp)}</div>
                 </div>
                 <div className="video-detail__tile-item">
-                    <div><img src={viewsIcon} /> {data.views}</div>
-                    <div><img src={likesIcon} /> {data.likes}</div>
+                    <div><img src={viewsIcon} alt='Views' /> {data.views}</div>
+                    <div><img src={likesIcon} alt='Likes' /> {data.likes}</div>
                 </div>
             </div>
+            <div className='divider'></div>
+
         </div>
     )
 }
