29c6c4921a851c1574d6df64dd83e565080b7f24
ganiular <ganiular@gmail.com>
2024-02-25 01:48:46 +0100

    handle form input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,14 +3,32 @@ import Avater from '../Avater/Avater';
 import CommentButton from '../Button/CommentButton';
 import './Form.scss';
 
-function CommentForm() {
+function CommentForm({ addComment }) {
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const form = event.target;
+        const commentInput = form.comment;
+
+        if (!commentInput.value) {
+            commentInput.classList.add('error');
+        } else {
+            console.log(commentInput.value);
+            form.reset();
+        }
+    }
+
+    function removeError(event) {
+        event.target.classList.remove('error');
+    }
+
     return (
-        <form className="form" action="" method="post">
+        <form className="form" action="" method="post" onSubmit={handleSubmit}>
             <label className='form__header'>JOIN THE CONVERSATION</label>
             <div className="form__body">
                 <Avater image={profileImage} />
                 <div className='form__fields'>
-                    <textarea name='comment' placeholder='Add a new comment' />
+                    <textarea name='comment' placeholder='Add a new comment' onChange={removeError} />
                     <CommentButton />
                 </div>
             </div>
