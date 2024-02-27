d083983a327d3d9b29c2b45e48c18b796c7c3d53
ganiular <ganiular@gmail.com>
2024-02-27 16:15:27 +0100

    Add page route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import HomePage from './pages/Home/Home';
+
 
 function App() {
 
   return (
-    <div className="App">
+    <BrowserRouter className="App">
       <Header />
-      <HomePage />
-    </div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
